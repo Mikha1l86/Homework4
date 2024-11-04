0709c67a10583e322d07d47bd7b8bea998db4b81
Mikha1l86 <misha.telichuk.86@gmail.com>
2024-11-05 01:02:44 +0500

    Сдаю ДЗ5 исправлена двойная вложенность задачи 1 задача 5 корректно работает, при выборе других чисел, выдает сообщение о неверном номере месяца.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,15 @@ public class Main {
         //Задача 1
         System.out.println("Задача 1");
 
-        int clientOS = 1;
-        if (clientOS != 0) {
-            if (clientOS == 1) {
-                System.out.println("Установите версию приложения для Android по ссылке.");
-            }
-        } else {
+        int clientOS = 0;
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
-    //Задача 2
-    System.out.println("Задача 2");
+        //Задача 2
+        System.out.println("Задача 2");
 
         clientOS = 1;
         int clientDeviceYear = 2014;
@@ -27,32 +25,32 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
-    //Задача 3
-    System.out.println("Задача 3");
+        //Задача 3
+        System.out.println("Задача 3");
 
         int year = 2021;
-        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)){
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
             System.out.println(year + " год является высокосным.");
         } else {
             System.out.println(year + " год не является высокосным");
         }
 
-    //Задача 4
-    System.out.println("Задача 4");
+        //Задача 4
+        System.out.println("Задача 4");
 
         int deliveryDistance = 95;
-        if (deliveryDistance <= 20){
+        if (deliveryDistance <= 20) {
             System.out.println("Для доставки карты потребуется: 1 день.");
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
             System.out.println("Для доставки карты потребуется: 2 дня.");
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
             System.out.println("Для доставки карты потребуется: 3 дня.");
-        }else {
+        } else {
             System.out.println("Доставки нет!");
-    }
+        }
 
-    //Задача 5
-    System.out.println("Задача 5");
+        //Задача 5
+        System.out.println("Задача 5");
 
         int monthNumber = 12;
         switch (monthNumber) {
@@ -80,4 +78,4 @@ public class Main {
                 System.out.println("Неверный номер месяца.");
         }
     }
-    }
+}
